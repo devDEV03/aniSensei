331d80b025e0b2f234a9938eaaddafeabc391f55
Dev Singhal <singhaldev26@gmail.com>
2024-10-28 14:40:17 +0530

    made the shimmer change

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,29 +1,29 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { lazy, Suspense } from "react";
+import { MovieShimmer } from "./Shimmer";
 
 const Login  = lazy(() => import("./Login"));
 const Browse = lazy(() => import("./Browse"));
 const Movie  = lazy(() => import("./Movie"));
 const StarredMovies = lazy(() => import("./StarredMovies"));
-const Shimmer = lazy(() => import("./Shimmer"));
 
 
 const Body = () => {
   const AppRouter = createBrowserRouter([
     {
       path: "/",
-      element: <Suspense fallback={<Shimmer />}><Login /></Suspense>,
+      element: <Suspense fallback={<MovieShimmer/>}><Login /></Suspense>,
     },
     {
       path: "/browse",
-      element: <Suspense fallback={<Shimmer />}><Browse /></Suspense>,
+      element: <Suspense fallback={<MovieShimmer />}><Browse /></Suspense>,
     },{
       path: "/starred",
-      element: <Suspense fallback={<Shimmer />}><StarredMovies /></Suspense>,
+      element: <Suspense fallback={<MovieShimmer />}><StarredMovies /></Suspense>,
     }, 
     {
       path: "/movie/:id",
-      element: <Suspense fallback={<Shimmer />}><Movie /></Suspense>,
+      element: <Suspense fallback={<MovieShimmer />}><Movie /></Suspense>,
     }
   ]);
 
